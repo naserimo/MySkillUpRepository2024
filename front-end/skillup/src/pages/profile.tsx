fa0038f1dbb337465c4e74b50908b2f0b664bff1
naserimo <167413045+naserimo@users.noreply.github.com>
2024-10-28 18:27:19 +0100

    Update profile.tsx

--- front-end/skillup/src/pages/profile.tsx
+++ front-end/skillup/src/pages/profile.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import './styles.css'; // Assuming styles.css is in the same folder
+import '../styles/styles.css';
+
 
 const Profile: React.FC = () => {
     // Example state for controlled inputs
