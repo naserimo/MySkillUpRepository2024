b6608650305d6630baa20a17f1f661ead9045479
naserimo <167413045+naserimo@users.noreply.github.com>
2024-10-28 20:45:32 +0100

    Update login.tsx

--- front-end/skillup/src/pages/login.tsx
+++ front-end/skillup/src/pages/login.tsx
@@ -22,7 +22,7 @@ const Login: React.FC = () => {
                 </div>
                 <nav>
                     <ul>
-                        <li><a href="home.tsx">Home</a></li>
+                        <li><a href="index.tsx">Home</a></li>
                         <li><a href="profile.tsx">Profile</a></li>
                         <li><a href="courses.tsx">Courses</a></li>
                     </ul>
